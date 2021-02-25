c819057d174ddd58ab6a8cd677c67c11eb183b94
clariones <clariones@163.com>
2021-02-25 10:10:30 +0800

    daily work

--- project_xt20/cla/edg/project/xt20/pageflow/Q06_2_Buyer.java
+++ project_xt20/cla/edg/project/xt20/pageflow/Q06_2_Buyer.java
@@ -12,7 +12,7 @@ public class Q06_2_Buyer extends PieceOfScript {
                 .comments("查询一个卖家对应的所有买家,连同其持瓶情况")
                 .do_it_as()
                 .where(MODEL.merchant().supplyRelationshipListAsCustomer().supplier().eq("${merchant id}"),
-                        MODEL.merchant().organizationIdentityList().name().like("${search key}").optional())
+                        MODEL.merchant().name().like("${search key}").optional())
                 .wants(MODEL.merchant().customerCylinderListAsCustomer().cylinder(), MODEL.merchant().organizationIdentityList())
 
             .query(MODEL.merchant()).list_of("buyer of merchant for management").pagination().with_string("merchant id").with_string("search key")
